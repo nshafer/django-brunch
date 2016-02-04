93050e19bfc9d618ac903a42af64d2b0967a5c90
Nathan Shafer <nate@torzo.com>
2016-02-04 13:41:17 -0700

    Change BRUNCH_CMD to be set more explicitly instead of string splitting magic

--- brunch/conf.py
+++ brunch/conf.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-settings.BRUNCH_CMD = getattr(settings, 'BRUNCH_CMD', "/usr/bin/env brunch")
+settings.BRUNCH_CMD = getattr(settings, 'BRUNCH_CMD', ("/usr/bin/env", "brunch"))
+settings.BRUNCH_SHELL = getattr(settings, 'BRUNCH_SHELL', False)
 settings.BRUNCH_SERVER = getattr(settings, 'BRUNCH_SERVER', False)
 
 if not hasattr(settings, 'BRUNCH_DIR'):
--- brunch/management/commands/runserver.py
+++ brunch/management/commands/runserver.py
@@ -3,6 +3,7 @@ import subprocess
 
 from django.contrib.staticfiles.management.commands.runserver import Command \
     as StaticfilesRunserverCommand
+from django.utils import six
 
 from brunch.conf import settings
 from brunch.utils import check_pid
@@ -18,12 +19,30 @@ class Command(StaticfilesRunserverCommand):
     def start_brunch(self):
         self.stdout.write("Starting brunch in {}".format(settings.BRUNCH_DIR))
 
-        brunch_args = settings.BRUNCH_CMD.split() + ["watch", "--stdin"]
-        if settings.BRUNCH_SERVER:
-            brunch_args.append('--server')
-        self.stdout.write("Using {}".format(" ".join(brunch_args)))
+        cmd = settings.BRUNCH_CMD
+        shell = settings.BRUNCH_SHELL
 
-        brunch_process = subprocess.Popen(brunch_args, cwd=settings.BRUNCH_DIR,
+        args = ["watch", "--stdin"]
+        if settings.BRUNCH_SERVER:
+            args.append('--server')
+
+        if isinstance(cmd, six.string_types):
+            if shell:
+                cmd = " ".join([cmd] + args)
+            else:
+                cmd = [cmd] + args
+        elif not isinstance(cmd, list):
+            cmd = list(cmd) + args
+        else:
+            cmd += args
+
+        if shell and not isinstance(cmd, six.string_types):
+            print("WARNING: BRUNCH_SHELL is true, but BRUNCH_CMD is not a string, attempting to fix")
+            cmd = " ".join(cmd)
+
+        self.stdout.write("Using {}{}".format("shell: " if shell else "", cmd if shell else " ".join(cmd)))
+
+        brunch_process = subprocess.Popen(cmd, cwd=settings.BRUNCH_DIR, shell=settings.BRUNCH_SHELL,
                                           stdin=subprocess.PIPE, stdout=self.stdout, stderr=self.stderr,)
 
         self.stdout.write('Brunch pid: {}'.format(brunch_process.pid))
